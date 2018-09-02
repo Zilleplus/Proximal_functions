474156cb25df83435e95b3b9b61035f6a4d5c69c
Manish <manish.krishanlal@esat.kuleuven.be>
2018-09-02 18:41:13 +0200

    Add files via upload

---
+++ include/indicator_intersection.h
@@ -0,0 +1,25 @@
+#ifndef INDICATOR_BOX_HYPERPLANE_H
+#define INDICATOR_BOX_HYPERPLANE_H
+
+#include "../globals/globals.h"
+
+struct indicator_intersection{
+    unsigned int dimension;
+    real_t lower_bound;
+    real_t upper_bound;
+    real_t inf;
+    real_t hyperplane_rhs;
+  
+};
+
+
+//box-halfspace ingredients
+struct indicator_bin* init_indicator_intersection_hyperplane(\
+    const unsigned int dimension_,const real_t lower_bound,const real_t upper_bound, const halfspace_rhs);
+
+//prox box-halfspace
+real_t prox_indicator_intersection_hyperplane(const struct indicator_intersection* data,real_t* input, real_t gamma);
+
+#endif
+
+
